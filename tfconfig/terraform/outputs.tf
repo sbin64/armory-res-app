f0e5ccbfcaffa2a8625a87917ae49de303982547
stackgen-automation <automation@stackgen.com>
2025-01-23 19:38:43 +0000

    Automated commit

---
+++ tfconfig/terraform/outputs.tf
@@ -0,0 +1,10 @@
+
+output "aws_iam_role_armory_res_app_role_arn" {
+  value = module.stackgen_f44f06f0-3481-5f25-bf71-2f2ce6f01b3b.arn
+  sensitive = false
+}
+
+output "aws_iam_role_armory_res_app_role_name" {
+  value = module.stackgen_f44f06f0-3481-5f25-bf71-2f2ce6f01b3b.name
+  sensitive = false
+}
